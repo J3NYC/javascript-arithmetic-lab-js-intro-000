293ad193145ccbc261084f1a9214ae7d94a0decc
Joseph Autorino <staircasesoul@gmail.com>
2021-04-05 13:41:52 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,21 +1,21 @@
 function add(a, b) {
-  return a + b 
+  return a + b
 }
 
 function subtract(a,b) {
-  return a - b 
+  return a - b
 }
 
 function divide(a,b) {
-  return a / b 
+  return a / b
 }
 
 function multiplly(a,b) {
-  return a * b 
+  return a * b
 }
 
 function increment(n) {
-  return n += 1 
+  return n += 1
 }
 
 function decrement(n) {
